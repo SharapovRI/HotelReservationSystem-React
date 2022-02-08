a871b109ff5d50db2d9a4e399b2f9b014dd33c97
Rodion <radzivon.sharapau@gmail.com>
2022-02-08 10:42:39 +0300

    Added time edit functionality

--- src/newModules/MyOrdersPage/OrdersList/OrderListItem/OrderListItem/OrderListItem.jsx
+++ src/newModules/MyOrdersPage/OrdersList/OrderListItem/OrderListItem/OrderListItem.jsx
@@ -5,14 +5,13 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import ClockPicker from '@mui/lab/ClockPicker';
 import StaticTimePicker from '@mui/lab/StaticTimePicker';
-import './OrderListItem.scss';
 import moment from 'moment';
+import './OrderListItem.scss';
+import putOrderTime from '../../../../../api/apiRequests/putOrderTime';
 
 const OrderListItem = ({ orderGroup }) => {
     const [hotel, setHotel] = useState();
-    const [date, setDate] = useState(new Date());
     const [arrivalDate, setArrivalDate] = useState(null);
-    const [arrivalTime, setArrivalTime] = useState();
 
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
@@ -22,12 +21,29 @@ const OrderListItem = ({ orderGroup }) => {
         const order = orderGroup?.orders[0];
         const formatString = 'ddd, MMM D. YYYY';
         const arrival_date = new Date(order?.checkInTime);
-        const arrival_time = moment(arrival_date.toJSON()).format('hh:mm a');;
-        const arrival = moment(arrival_date.toJSON()).format(formatString);
 
-        setArrivalTime(arrival_time);
         setArrivalDate(arrival_date);
     }, [orderGroup])
+
+    function saveNewArrivalTime() {
+        async function postNewTime() {
+            const payload = {
+                id: orderGroup.id,
+                checkInTime: arrivalDate,
+            }
+            const result = await putOrderTime(payload);
+        }
+
+        postNewTime();
+        handleClose();
+    }
+
+    function cancelClick() {
+        const order = orderGroup?.orders[0];
+        const arrival_date = new Date(order?.checkInTime);
+        setArrivalDate(arrival_date);
+        handleClose();
+    }
 
     return (
         <div className="order_list_item">
@@ -37,29 +53,39 @@ const OrderListItem = ({ orderGroup }) => {
             <div className="order_list_main">
                 <OrderInfo orderGroup={orderGroup} hotel={hotel} setHotel={setHotel} />
             </div>
-            <div className="oli_buttons">
-                <button onClick={handleOpen}>Edit arrival time</button>
-            </div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-            >
-                <div className="oli_time_edit_modal">
-                    <div className="oli_tem_header"></div>
-                    <div className="oli_tem_body">
-                        <LocalizationProvider dateAdapter={AdapterDateFns}>
-                            <StaticTimePicker
-                                date={arrivalDate}
-                                onChange={(newDate) => setArrivalDate(newDate)}
-                                minTime={new Date(0, 0, 0, 14)}
-                                ampm
-                                orientation="landscape"
-                                openTo="minutes"
-                            />
-                        </LocalizationProvider>
+            {orderGroup?.orders[0].checkInTime > new Date().toJSON() &&
+                <>
+                    <div className="oli_buttons">
+                        <button onClick={handleOpen}>Edit arrival time</button>
                     </div>
-                </div>
-            </Modal>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                    >
+                        <div className="oli_time_edit_modal">
+                            <div className="oli_tem_header"></div>
+                            <div className="oli_tem_body">
+                                <div className="time_picker">
+                                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                        <StaticTimePicker
+                                            date={arrivalDate}
+                                            onChange={(newDate) => setArrivalDate(newDate)}
+                                            minTime={new Date(0, 0, 0, 14)}
+                                            ampm
+                                            orientation="landscape"
+                                            openTo="minutes"
+                                        />
+                                    </LocalizationProvider>
+                                </div>
+                                <div className="oli_tem_buttons">
+                                    <button className='oli_tem_button' onClick={cancelClick}>Cancel</button>
+                                    <button className='oli_tem_button' onClick={saveNewArrivalTime}>Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </Modal>
+                </>
+            }
         </div>
     )
 }
